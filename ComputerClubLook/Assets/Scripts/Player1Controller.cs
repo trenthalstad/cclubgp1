e3892e0cef26eb9e1c4046ca2113a4b7f9e76189
trenthalstad <trentonmhasltad@fvcc.edu>
2015-10-23 17:56:25 -0600

    Scene
    Added a player and movement and started working the camera.

---
+++ ComputerClubLook/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0F, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+}
